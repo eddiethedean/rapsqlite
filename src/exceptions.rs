5ff4cb05ac5282aaec486b95c0c36047eac2d502
Odos Matthews <odosmatthews@Odoss-Mac-mini.local>
2026-01-28 20:34:30 -0500

    fix: align exception inheritance with DB-API
    Make DatabaseError inherit from Error and have OperationalError/ProgrammingError/
    IntegrityError inherit from DatabaseError. Also make rapsqlite.ValueError inherit
    from Python's ValueError so it behaves like a parameter validation error.

--- src/exceptions.rs
+++ src/exceptions.rs
@@ -2,12 +2,13 @@
 
 use pyo3::create_exception;
 use pyo3::exceptions::PyException;
+use pyo3::exceptions::PyValueError;
 
 // Exception classes matching aiosqlite API (ABI3 compatible)
 create_exception!(_rapsqlite, Error, PyException);
 create_exception!(_rapsqlite, Warning, PyException);
-create_exception!(_rapsqlite, DatabaseError, PyException);
-create_exception!(_rapsqlite, OperationalError, PyException);
-create_exception!(_rapsqlite, ProgrammingError, PyException);
-create_exception!(_rapsqlite, IntegrityError, PyException);
-create_exception!(_rapsqlite, ValueError, PyException);
+create_exception!(_rapsqlite, DatabaseError, Error);
+create_exception!(_rapsqlite, OperationalError, DatabaseError);
+create_exception!(_rapsqlite, ProgrammingError, DatabaseError);
+create_exception!(_rapsqlite, IntegrityError, DatabaseError);
+create_exception!(_rapsqlite, ValueError, PyValueError);
